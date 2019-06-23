6c33d05cddf70b99206746fd48c957247b4f7594
Barry Burch <barry@sbcglobal.net>
2019-06-23 10:36:19 -0500

    Continue service & deployment development

--- fotaas-internal/app/telemetry/models/db.go
+++ fotaas-internal/app/telemetry/models/db.go
@@ -2,30 +2,73 @@ package models
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
+	"go.uber.org/zap"
+
+	logging "github.com/bburch01/FOTAAS/fotaas-internal/pkg/logging"
 )
 
 var db *sql.DB
+var logger *zap.Logger
 
+/*
 func init() {
 	if err := godotenv.Load(); err != nil {
 		log.Printf("godotenv error: %v", err)
 	}
 }
+*/
+
+func init() {
+
+	var lm logging.LogMode
+	var err error
+
+	// Loads values from .env into the system.
+	// NOTE: the .env file must be present in execution directory which is a
+	// deployment issue that will be handled via docker/k8s in production but
+	// the .env file may need to be manually copied into the execution directory
+	// during testing.
+	if err = godotenv.Load(); err != nil {
+		log.Panicf("failed to load environment variables with error: %v", err)
+	}
+
+	if lm, err = logging.LogModeForString(os.Getenv("LOG_MODE")); err != nil {
+		log.Panicf("failed to initialize logging subsystem with error: %v", err)
+	}
+
+	if logger, err = logging.NewLogger(lm, os.Getenv("LOG_DIR"), os.Getenv("LOG_FILE_NAME")); err != nil {
+		log.Panicf("failed to initialize logging subsystem with error: %v", err)
+	}
+
+}
 
 func InitDB() error {
 	dbDriver := os.Getenv("DB_DRIVER")
 	dbHost := os.Getenv("DB_HOST")
-	dbPort := os.Getenv("DB_PORT")
+	//dbPort := os.Getenv("DB_PORT")
 	dbUser := os.Getenv("DB_USER")
 	dbPass := os.Getenv("DB_PASSWORD")
 	dbName := os.Getenv("TELEMETRY_SERVICE_DB_NAME")
 	var err error
-	db, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHost+":"+dbPort+")"+"/"+dbName)
+
+	logger.Debug(fmt.Sprintf("DB_DRIVER: %v DB_HOST: %v DB_USER: %v DB_PASSWORD: %v ANALYSIS_SERVICE_DB_NAME: %v",
+		dbDriver, dbHost, dbUser, dbPass, dbName))
+
+	dbConURL := dbUser + ":" + dbPass + "@tcp(" + dbHost + ")" + "/" + dbName
+
+	logger.Debug(fmt.Sprintf("Database Connection URL: %v", dbConURL))
+
+	//MySQLDB, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHost+")"+"/"+dbName)
+	db, err = sql.Open(dbDriver, dbConURL)
+
+	//db, err = sql.Open(dbDriver, dbUser+":"+dbPass+"@tcp("+dbHost+")"+"/"+dbName)
+
 	if err != nil {
 		return err
 	}
